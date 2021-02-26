3de53620d8cc21f46452e0a6e83cea2b4990e49c
gxb <974825649@qq.com>
2021-02-26 09:55:23 +0800

    1

---
+++ common/comJs/forIn.js
@@ -0,0 +1,13 @@
+let obj = {
+    name: '魂魄',
+    age: '18'
+}
+
+for(let item in obj){
+    console.log(`item = ${item}`);
+    console.log(obj[item]);
+}
+
+// for(let item2 of obj){
+//     console.log(`item2 = ${item2}`);
+// }
